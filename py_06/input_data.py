1b589bc1ec06033a1e8c9aa038e6962bacf95e23
Michael Schneider <sesenion@gmail.com>
2023-12-06 20:19:14 +0100

    Input from day6 as python

---
+++ py_06/input_data.py
@@ -0,0 +1,18 @@
+input_data = [
+    {
+        "time": 51,
+        "distance": 222,
+    },
+    {
+        "time": 92,
+        "distance": 2031,
+    },
+    {
+        "time": 68,
+        "distance": 1126,
+    },
+    {
+        "time": 90,
+        "distance": 1225,
+    },
+]
